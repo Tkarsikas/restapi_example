792192796b25c50992a0a94ab19de238359dfef2
Tommi <tommi.karsi@gmail.com>
2026-01-12 00:00:40 +0200

    login endpoint done and tested

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var studentRouter = require('./routes/student');
+var loginRouter = require('./routes/login');
+const jwt = require('jsonwebtoken');
 
 var app = express();
 
@@ -15,6 +17,26 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+//suojatut reitit
+app.use(authenticateToken);
 app.use('/student', studentRouter);
+app.use('/login', loginRouter);
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    // Extract token by removing "Bearer " prefix from "Bearer "
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+    jwt.verify(token, process.env.MY_TOKEN, function(err, user) {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    })
+  }
+
 
 module.exports = app;
