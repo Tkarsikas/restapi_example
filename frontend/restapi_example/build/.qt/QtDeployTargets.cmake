57f4279a03ee65523ec993596864a40c75a1ef84
Tommi <tommi.karsi@gmail.com>
2026-01-12 06:03:55 +0200

    poistin turhat tiedostot

--- frontend/restapi_example/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_frontend_FILE C:/GIT_Repositoryt/restapi_example/frontend/restapi_example/build/frontend.exe)
-set(__QT_DEPLOY_TARGET_frontend_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_frontend_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
